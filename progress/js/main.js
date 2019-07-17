453991a5a38b6612b640e2b9895b44264bd647e5
laffkadgan <52545755+laffkadgan@users.noreply.github.com>
2019-07-17 18:34:59 +0300

    Add files via upload

---
+++ progress/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".slider").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000
+    });
+});
+
+
+
+   $(function(){
+            $('.gallery').slick({
+                vertical: true,
+                verticalSwiping: true,
+                slidesToShow: 3,
+                autoplay: true,
+                slidesToShow: 3,
+            });
+        });
+        
